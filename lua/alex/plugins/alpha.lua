7489f78bce6a34bc4859433cca7745d759d842e7
Alex <alexmonac13@gmail.com>
2026-06-05 15:16:08 +0200

    chore: update alpha menu

--- lua/alex/plugins/alpha.lua
+++ lua/alex/plugins/alpha.lua
@@ -9,17 +9,22 @@ return {
 		local dashboard = require("alpha.themes.dashboard")
 		local ascii = require("ascii")
 
+		local function i(n)
+			return vim.fn.nr2char(n)
+		end
+
 		-- Set header
 		dashboard.section.header.val = ascii.get_random("text", "neovim")
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>AutoSession restore<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("SPC ee", i(0xf115) .. "  File Explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC ff", i(0xf0fbc) .. "  Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC fs", i(0xf002) .. "  Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC xt", i(0xf00c) .. "  TODOs", "<cmd>Trouble todo toggle<CR>"),
+			dashboard.button("SPC D", i(0xf188) .. "  Diagnostics", "<cmd>Telescope diagnostics<CR>"),
+			dashboard.button("l", i(0xf487) .. "  Lazy", "<cmd>Lazy<CR>"),
+			dashboard.button("q", i(0xf011) .. "  Quit", "<cmd>qa<CR>"),
 		}
 
 		-- Send config to alpha
