108cd3204a535fb17e24b37be937d68290db7b60
Alex <alexmonac13@gmail.com>
2026-06-08 15:19:51 +0200

    chore: comment copilot

--- lua/alex/plugins/copilot.lua
+++ lua/alex/plugins/copilot.lua
@@ -1,59 +1,60 @@
-return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				panel = {
-					enabled = true,
-					auto_refresh = false,
-					keymap = {
-						jump_prev = "[[",
-						jump_next = "]]",
-						accept = "<CR>",
-						refresh = "gr",
-						open = "<M-CR>",
-					},
-					layout = {
-						position = "bottom",
-						ratio = 0.4,
-					},
-				},
-				suggestion = {
-					enabled = true, -- Réactivé pour avoir le ghost text
-					auto_trigger = true,
-					debounce = 75,
-					keymap = {
-						accept = "<Tab>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-				filetypes = {
-					yaml = false,
-					markdown = false,
-					help = false,
-					gitcommit = false,
-					gitrebase = false,
-					hgcommit = false,
-					svn = false,
-					cvs = false,
-					["."] = false,
-				},
-				copilot_node_command = "node",
-				server_opts_overrides = {},
-			})
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		dependencies = { "zbirenbaum/copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup()
-		end,
-	},
-}
+return {}
+-- return {
+-- 	{
+-- 		"zbirenbaum/copilot.lua",
+-- 		cmd = "Copilot",
+-- 		event = "InsertEnter",
+-- 		config = function()
+-- 			require("copilot").setup({
+-- 				panel = {
+-- 					enabled = true,
+-- 					auto_refresh = false,
+-- 					keymap = {
+-- 						jump_prev = "[[",
+-- 						jump_next = "]]",
+-- 						accept = "<CR>",
+-- 						refresh = "gr",
+-- 						open = "<M-CR>",
+-- 					},
+-- 					layout = {
+-- 						position = "bottom",
+-- 						ratio = 0.4,
+-- 					},
+-- 				},
+-- 				suggestion = {
+-- 					enabled = true, -- Réactivé pour avoir le ghost text
+-- 					auto_trigger = true,
+-- 					debounce = 75,
+-- 					keymap = {
+-- 						accept = "<Tab>",
+-- 						accept_word = false,
+-- 						accept_line = false,
+-- 						next = "<M-]>",
+-- 						prev = "<M-[>",
+-- 						dismiss = "<C-]>",
+-- 					},
+-- 				},
+-- 				filetypes = {
+-- 					yaml = false,
+-- 					markdown = false,
+-- 					help = false,
+-- 					gitcommit = false,
+-- 					gitrebase = false,
+-- 					hgcommit = false,
+-- 					svn = false,
+-- 					cvs = false,
+-- 					["."] = false,
+-- 				},
+-- 				copilot_node_command = "node",
+-- 				server_opts_overrides = {},
+-- 			})
+-- 		end,
+-- 	},
+-- 	{
+-- 		"zbirenbaum/copilot-cmp",
+-- 		dependencies = { "zbirenbaum/copilot.lua" },
+-- 		config = function()
+-- 			require("copilot_cmp").setup()
+-- 		end,
+-- 	},
+-- }
