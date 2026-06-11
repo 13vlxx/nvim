97b774bdd7f3c4aed98a8d91401ce5514e1e758b
Alex <alexmonac13@gmail.com>
2026-06-11 15:37:29 +0200

    feat: add a default formatter if no biome or prettier configured

--- lua/alex/plugins/conform.lua
+++ lua/alex/plugins/conform.lua
@@ -10,50 +10,42 @@ return {
 				python = { "ruff_organize_imports", "ruff_format" },
 				rust = { "rustfmt" },
 				go = { "goimports", "gofumpt" },
-				javascript = { "biome", "prettier", stop_after_first = true },
-				typescript = { "biome", "prettier", stop_after_first = true },
-				javascriptreact = { "biome", "prettier", stop_after_first = true },
-				typescriptreact = { "biome", "prettier", stop_after_first = true },
-				html = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				json = { "biome", "prettier", stop_after_first = true },
-				jsonc = { "biome", "prettier", stop_after_first = true },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				prisma = { "prettier" },
+				javascript = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				typescript = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				javascriptreact = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				typescriptreact = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				html = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				css = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				scss = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				json = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				jsonc = { "biome", "prettier", "prettier_global", stop_after_first = true },
+				yaml = { "prettier", "prettier_global", stop_after_first = true },
+				markdown = { "prettier", "prettier_global", stop_after_first = true },
+				prisma = { "prettier", "prettier_global", stop_after_first = true },
 			},
 			formatters = {
-				-- Prettier : utilise UNIQUEMENT la config du projet, sans override
 				prettier = {
-					-- Ne pas ajouter d'args, laisse la config du projet décider
-					require_cwd = true, -- Utilise le working directory du projet
+					require_cwd = true,
 				},
-				-- Biome : utilise UNIQUEMENT la config du projet
 				biome = {
 					require_cwd = true,
+				},
+				prettier_global = {
+					command = vim.fn.stdpath("data") .. "/mason/bin/prettier",
+					args = { "--stdin-filepath", "$FILENAME" },
+					stdin = true,
 				},
 			},
 		})
 
-		-- Format à la sauvegarde
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			callback = function(args)
 				conform.format({
 					bufnr = args.buf,
 					lsp_fallback = true,
-					timeout_ms = 1000,
+					timeout_ms = 500,
 				})
 			end,
 		})
-
-		-- Raccourci manuel pour formater
-		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range" })
 	end,
 }
